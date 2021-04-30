ab49d68a4e480ea6437ea9730311801253d20e09
Bhavana Mahendraker <43737332+BhavanaMahendraker@users.noreply.github.com>
2021-04-29 21:36:53 -0700

    Update StarbucksRegisterRepository.java

--- starbucks-api-final-project/src/main/java/com/example/starbucksapi/StarbucksRegisterRepository.java
+++ starbucks-api-final-project/src/main/java/com/example/starbucksapi/StarbucksRegisterRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 interface StarbucksRegisterRepository extends JpaRepository<StarbucksRegister, Long> {
-    //StarbucksRegister findByRegister(String regid);
-    //@Transactional StarbucksRegister deleteByRegid(String regid);
+    
 
 }
